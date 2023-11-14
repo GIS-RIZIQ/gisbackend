24187919a71a5a20f947180a3c596a145999a49d
FarhanRiziq01 <farhanriziq01@gmail.com>
2023-11-14 12:31:26 +0700

    test

--- geojson_test.go
+++ geojson_test.go
@@ -19,14 +19,14 @@ func TestGeneratePaseto(t *testing.T) {
 	//privateKey, publicKey := watoken.GenerateKey()
 	//fmt.Println(privateKey)
 	//fmt.Println(publicKey)
-	hasil, err := watoken.Encode("haris", "PrivateKey")
+	hasil, err := watoken.Encode("riziq", "PrivateKey")
 	fmt.Println(hasil, err)
 }
 
 func TestUpdateData(t *testing.T) {
 	data := LonLatProperties{
 		Type:   "Polygon",
-		Name:   "cihuy",
+		Name:   "aci",
 		Volume: "1",
 	}
 	up := UpdateNameGeo("MONGOSTRING", "gis", context.Background(), data)
@@ -36,7 +36,7 @@ func TestUpdateData(t *testing.T) {
 func TestDeleteDataGeo(t *testing.T) {
 	data := LonLatProperties{
 		Type:   "Point",
-		Name:   "gege",
+		Name:   "ggwp",
 		Volume: "1",
 	}
 	up := DeleteDataGeo("MONGOSTRING", "gis", context.Background(), data)
@@ -44,10 +44,10 @@ func TestDeleteDataGeo(t *testing.T) {
 }
 
 func TestInsertUser(t *testing.T) {
-	conn := GetConnectionMongo("MONGOSTRING", "gis")
-	pass, _ := pasproj.HashPass("haris")
+	conn := GetConnectionMongo("MONGOSTRING", "nyoba")
+	pass, _ := pasproj.HashPass("riziq")
 	data := RegisterStruct{
-		Username: "haris",
+		Username: "coba",
 		Password: pass,
 	}
 	ins := InsertUserdata(conn, data.Username, data.Password)
